b4f5175066689e5485351b636a73e4e7d859be77
Savannah Ostrowski <savannah@python.org>
2025-11-16 19:50:31 -0800

    👷 Add workflows

---
+++ frontend/eslint.config.js
@@ -0,0 +1,31 @@
+import js from '@eslint/js';
+import tseslint from '@typescript-eslint/eslint-plugin';
+import tsparser from '@typescript-eslint/parser';
+import solid from 'eslint-plugin-solid';
+
+export default [
+  js.configs.recommended,
+  {
+    files: ['src/**/*.{ts,tsx}'],
+    plugins: {
+      '@typescript-eslint': tseslint,
+      'solid': solid,
+    },
+    languageOptions: {
+      parser: tsparser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+      },
+    },
+    rules: {
+      ...tseslint.configs.recommended.rules,
+      ...solid.configs.recommended.rules,
+      '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+      '@typescript-eslint/no-explicit-any': 'warn',
+    },
+  },
+  {
+    ignores: ['dist/', 'node_modules/'],
+  },
+];
